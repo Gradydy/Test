67e25d0054d8408f6dc3c345b550af5837d0ed3a
Lezonn <lezonmail@gmail.com>
2022-01-20 19:44:23 +0700

    remove sales tabbed pane

--- src/View/MasterPage.java
+++ src/View/MasterPage.java
@@ -31,12 +31,6 @@ public class MasterPage extends JDialog {
     private JButton AdminDeleteBtn;
     private JTable AdminTbl;
 
-    private JTextField textField5;
-    private JTextField textField6;
-    private JTextField textField7;
-    private JTextField textField8;
-    private JButton insertButton1;
-
     private JTextField textField9;
     private JTextField textField10;
     private JTextField textField11;
@@ -54,15 +48,11 @@ public class MasterPage extends JDialog {
     private JButton deleteButton;
     private JButton updateButton1;
     private JButton deleteButton1;
-    private JButton updateButton2;
-    private JButton deleteButton2;
 
-    private JPanel SalesTab;
     private JPanel AdminTab;
     private JPanel ItemTab;
     private JTextField AdminPhonetxt;
     private JComboBox AdminRolecb;
-    private JTable SalesTbl;
     private JTable DistibutorTbl;
     private JTable ItemTbl;
 
@@ -103,31 +93,54 @@ public class MasterPage extends JDialog {
     // Method to list admin btn listener
     private void adminBtnListener(AdminTable adminTableModelTemp) {
         // Insert
-        AdminInsertBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                if(validateInputAdmin() == false) return;
-                String query = "INSERT INTO MsUser" +
-                        "(AuditedUserID, RoleID, Name, Phone, Username, Password) " +
-                        "VALUES(?, (SELECT RoleID FROM MsRole WHERE RoleName LIKE ?), ?, ?, ?, ?)";
-                PreparedStatement ps = con.preparedStatement(query);
-                try {
-                    ps.setInt(1, userSession.UserID);
-                    ps.setString(2, AdminRolecb.getSelectedItem().toString());
-                    ps.setString(3, AdminNametxt.getText());
-                    ps.setString(4, AdminPhonetxt.getText());
-                    ps.setString(5, AdminUsernametxt.getText());
-                    ps.setString(6, AdminPasswordtxt.getText());
-                    if(ps.executeUpdate() > 0) {
-                        clearAdminField();
-                        getAllUser();
-                        adminTableModelTemp.fireTableDataChanged();
-                    }
-
-                } catch (Exception e) {
-                    // TODO: handle exception
-                    e.printStackTrace();
+        AdminInsertBtn.addActionListener(ae -> {
+            if(validateInputAdmin() == false) return;
+            String query = "INSERT INTO MsUser" +
+                    "(AuditedUserID, RoleID, Name, Phone, Username, Password) " +
+                    "VALUES(?, (SELECT RoleID FROM MsRole WHERE RoleName LIKE ?), ?, ?, ?, ?)";
+            PreparedStatement ps = con.preparedStatement(query);
+            try {
+                ps.setInt(1, userSession.UserID);
+                ps.setString(2, AdminRolecb.getSelectedItem().toString());
+                ps.setString(3, AdminNametxt.getText());
+                ps.setString(4, AdminPhonetxt.getText());
+                ps.setString(5, AdminUsernametxt.getText());
+                ps.setString(6, AdminPasswordtxt.getText());
+                if(ps.executeUpdate() > 0) {
+                    clearAdminField();
+                    getAllUser();
+                    adminTableModelTemp.fireTableDataChanged();
                 }
+
+            } catch (Exception e) {
+                // TODO: handle exception
+                e.printStackTrace();
+            }
+        });
+
+        // Update item
+        AdminUpdateBtn.addActionListener(ae -> {
+            if(validateInputAdmin() == false) return;
+            String query = "INSERT INTO MsUser" +
+                    "(AuditedUserID, RoleID, Name, Phone, Username, Password) " +
+                    "VALUES(?, (SELECT RoleID FROM MsRole WHERE RoleName LIKE ?), ?, ?, ?, ?)";
+            PreparedStatement ps = con.preparedStatement(query);
+            try {
+                ps.setInt(1, userSession.UserID);
+                ps.setString(2, AdminRolecb.getSelectedItem().toString());
+                ps.setString(3, AdminNametxt.getText());
+                ps.setString(4, AdminPhonetxt.getText());
+                ps.setString(5, AdminUsernametxt.getText());
+                ps.setString(6, AdminPasswordtxt.getText());
+                if(ps.executeUpdate() > 0) {
+                    clearAdminField();
+                    getAllUser();
+                    adminTableModelTemp.fireTableDataChanged();
+                }
+
+            } catch (Exception e) {
+                // TODO: handle exception
+                e.printStackTrace();
             }
         });
 
